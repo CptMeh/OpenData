683225809ebb0f88aeb56f82347e143190b0c2d4
Cptmeh <ramon.naef@students.unibe.ch>
2023-05-25 12:52:38 +0200

    working tooltips and fixed the map so it's not upside down anymore

--- D3.js_implementation/app.js
+++ D3.js_implementation/app.js
@@ -1,15 +1,16 @@
 // Make this dynamic!
-const width = 750;
-const height = 600;
+
+const width = 800; 
+const height = 800; 
+// Maybe language?
+// let language = ...;
 
 // Add an SVG to the div with the ID 'map' and then sets its height and width.
 const svg = d3.select("#map")
               .append("svg")
               .attr("width", width)
               .attr("height", height)
-              //.style("curser", "move")
-              //.attr("preserveAspectRatio", "xMinYMax")
-              ; 
+              .attr("preserveAspectRatio", "xMinYMax"); 
 
 // Get the map- and TREE-data and check if everything worked. Then use the drawMap function to render the map.
 d3.queue()
@@ -17,72 +18,14 @@ d3.queue()
   .defer(d3.json, "https://data.geo.admin.ch/ch.bafu.landesforstinventar-kantone/landesforstinventar-kantone/landesforstinventar-kantone_2056.geojson")
   
   // This one is the TREE-Data
-  //.defer(d3.csv, "/Daten/TREE2_Data_IWI_Open_Data_Vorlesung_2023_label.csv")
+  .defer(d3.csv, "https://raw.githubusercontent.com/CptMeh/OpenData/main/Daten/TREE2_Data_IWI_Open_Data_Vorlesung_2023_label.csv")
 
   // Wait till all the data is ready
-  .await(function (error, map/*, data*/) {
+  .await(function (error, map, data) {
       if (error) {
         console.error("Somthing went wrong: " + error);
       } else {
-        drawMap(map/*, data*/);
+        drawMap(map, data);
       }
-  });
-
-
-/* Renders the map itself and (once we get it to work) can then add the TREE-Data to it. (not implemented ye ,:D)*/
-function drawMap(map, data) {
-
-  // The projection determines what kind of plane the map itself is projected on to (eg. onto a globe or a flat plain).
-  const projection = d3.geoIdentity().fitSize([width, height], map);
-
-  // Idk what dis doo yet :/ I think it renders the kantone?
-  const path = d3.geoPath().projection(projection);
-
-  svg.selectAll("path")
-    .data(map.features)
-    .enter()
-    .append("path")
-    .attr("name", function(d) {
-      return d.properties.name;
-    })
-    .attr("d", path) // Idk 
-    .style("fill", "grey") // Set the background color of the Kantons
-    .style("stroke", "white") // Set the color of the Kanton Borders
-    .style("stroke-width", 0.5) // Thickness of the borderlines
-
-    // Highlights the Kanton the mouse is hovering over by changing the style.
-    // It also shows a little text field 
-    .on("mouseover", function(d) {
-
-      // Remove previous text
-      svg.selectAll(".externalObject").remove(); 
-
-
-      // Highlight selected part
-      d3.select(this) 
-        .style("stroke", "while")
-        .style("stroke-width", 2)
-        .style("curser", "pointer"); // Last part doesn't work yet
-
-      // Add Text info about selected part
-      svg.append("foreignObject")
-        .attr("class", "externalObject")
-        .attr("x", 550 + "px") // Placement of box
-        .attr("y", 0 + "px")
-        .attr("width", 200) // Size of box
-        .attr("height", 100)
-        .append("xhtml:div")
-        .style("rotate", "180deg") // This is nessecary, because everything is upside down because the map is weird :/
-        .style("transform", "rotateY(180deg)") // Ditto
-        .html("<input type='text' id=new_x1 placeholder='" + d.properties.KantonName_de + " (" + d.properties.alternateName + ")" +"' onchange=update()></input>");
-              //+ "<input type='text' id=new_x2 placeholder='" + DATEN.....);
-
-    })
-
-    // De-highlight once curser leaves by changing back the style of the Kanton
-    .on("mouseout", function(d) {
-      d3.select(this)
-        .style("stroke", "white")
-        .style("stroke-width", 0.5);      
-    });
-}
+  }
+);
---
+++ D3.js_implementation/functions/map.js
@@ -0,0 +1,175 @@
+// Renders the map and can then add the TREE-Data to it.
+function drawMap(map, data /*,index */) {
+    let projection = d3.geoIdentity().reflectY(true).fitSize([width*0.9, height*0.9], map);// The projection determines what kind of plane the map itself is projected on to (eg. onto a globe or a flat plain).
+    let path = d3.geoPath().projection(projection);
+    
+    updateMap(map, data, "gender"); // Prepare the data that should be rendered (decided by user via selection panel)
+
+
+    let tooltip = d3.select("#map")
+                        .append("div")
+                        .attr("id", "tooltip")
+                        // Somehow the following is not added via id. find out why
+                        .style("opacity", 0)
+                        .attr("class", "tooltip")
+                        .style("background-color", "white")
+                        .style("border", "solid")
+                        .style("border-width", "2px")
+                        .style("border-radius", "5px")
+                        .style("padding", "5px")
+                        .style("position","absolute");
+
+    let color = d3.scaleSequential()
+                    .interpolator(d3.interpolateInferno)
+                    .domain([1,100])
+    
+    let mouseover = function(d) {
+        // Highlight selected part
+        d3.select(this) 
+        .style("stroke", "white")
+        .style("stroke-width", 2)
+        .style("cursor", "pointer");    // maybe use .classed("canton-highligted", true); ?       
+
+        /* Show tooltip
+        tooltip.style("opacity", 0.8)
+                .style("z-index", "0");*/
+    };
+
+    let current = null; // for enabling toggling of the tooltip box
+    let mouseclick = function(d) {
+        // Adds the text to the tooltip and updates the position on where the mouse is.
+        tooltip.html(createLabel(d))
+                .style("left", (d3.mouse(this)[0]+25) + "px")
+                .style("top", (d3.mouse(this)[1]) + "px");
+
+        current === this ? tooltip.style("opacity", 0.8) : tooltip.style("opacity", 0)
+        current = this;
+    };
+    
+    let mouseleave = function(d) {
+        // De-highlight once cursor leaves by changing back the style of the Kanton
+        d3.select(this)
+            .style("stroke", "white")
+            .style("stroke-width", 0.5);   
+
+        /* Hide tooltip
+        tooltip.style("opacity", 0)
+                .style("z-index", "900");*/
+    };
+
+    svg.selectAll("path")
+        .data(map.features)
+        .enter()
+        .append("path")
+            .attr("class", "canton")
+            .attr("id", function(d) { return d.properties.KantonId; })
+            .attr("d", path) 
+            .style("fill", "grey") // Set the background color of the Kantons
+            .style("stroke", "white") // Set the color of the Kanton Borders
+            .style("stroke-width", 0.5) // Thickness of the borderlines
+            .style("z-index", "950")
+        .on("mouseover", mouseover)
+        .on("click", mouseclick)
+        .on("mouseleave", mouseleave);
+}
+
+// Updates the map if the variables change.
+function updateMap(map, data, select) {
+    let dataByKanton = {};
+
+    data.forEach(function (d) {
+        let kanton = d["aes_canton"].includes("Basel") ? "beide Basel (BL/BS)" : d["aes_canton"];
+    
+        switch (select) {
+          case "gender" : prepareGender(d, dataByKanton, kanton); break;
+          case "immigratoin" : prepareImmigration(d, dataByKanton, kanton); break;
+          case "" : break;
+          default : break;
+        }
+      });
+
+    //always do this
+    prepareMapData(map, dataByKanton);
+}
+
+function createLabel(d) {
+  let properties = d.properties;
+  let details = properties.details;
+
+  return "<p><b>" + properties.KantonName_de + " (" + d.properties.alternateName + ")</b></p>" +
+        "<p>" + "Gesammtteilnehmer: " + details.both + "</p>" +
+        "<p>" + "Weibliche Teilnehmer: " + details.female + "</p>" +
+        "<p>" + "Männliche Teilnehmer: " + details.male + "</p>"/* +
+        "<p>" + "Immigrationstatus 1: " + details.immigration_1 + "</p>" +
+        "<p>" + "Immigrationstatus 2: " + details.immigration_2 + "</p>" +
+        "<p>" + "Immigrationstatus 3: " + details.immigration_3 + "</p>" +
+        "<p>" + "Percent Immigration 1: " + details.percent_immigration_1 + "</p>" +
+        "<p>" + "Percent Immigration 2: " + details.percent_immigration_2 + "</p>" +
+        "<p>" + "Percent Immigration 3: " + details.percent_immigration_3 + "</p>"*/;
+}
+
+
+function prepareImmigration(d, dataByKanton, kanton) {
+  let immigration = d["t0immig"]; // immigration status (1 - native, 2 - second gen, 3 - first gen, . - missing)
+  let IPW1 = d["t1wt"]; // Inverse Probability Weight - Wave 1
+
+  assignData(dataByKanton[kanton], {immigration_1: 0, immigration_2: 0, immigration_3: 0});
+  assignData(dataByKanton["sum"], {immigration_1: 0, immigration_2: 0, immigration_3: 0});
+
+  switch (immigration) {
+    case "Native (at least 1 parent born in Switzerland)": dataByKanton[kanton].immigration_1 += 1 * IPW1;
+    case "Second generation (respondent born in Switzerland, no parent born in Switzerland)": dataByKanton[kanton].immigration_2 += 1 * IPW1;
+    case "First generation (respondent and parent(s) born abroad)": dataByKanton[kanton].immigration_3 += 1 * IPW1;
+    case ".": dataByKanton[kanton].immigration_4 += 1 * IPW1;
+  }
+
+  // needed for converting the totals to percent
+  total_immigration_weighted = dataByKanton[kanton].immigration_1 
+                                + dataByKanton[kanton].immigration_2 
+                                + dataByKanton[kanton].immigration_3 
+                                + dataByKanton[kanton].immigration_4;
+
+  dataByKanton[kanton].percent_immigration_1 = (100 / total_immigration_weighted) * dataByKanton[kanton].immigration_1;
+  dataByKanton[kanton].percent_immigration_2 = (100 / total_immigration_weighted) * dataByKanton[kanton].immigration_2;
+  dataByKanton[kanton].percent_immigration_3 = (100 / total_immigration_weighted) * dataByKanton[kanton].immigration_3;
+}
+
+
+
+function prepareGender(d, dataByKanton, kanton) {
+  let gender = d["t0sex"];
+
+  dataByKanton[kanton] = assignData(dataByKanton[kanton], {both: 0, female: 0, male: 0,});
+  dataByKanton["sum"] = assignData(dataByKanton["sum"], {both: 0, female: 0, male: 0,});
+
+  if (gender === "Female") {
+    dataByKanton[kanton].female += 1;
+    dataByKanton["sum"].female += 1;
+
+  } else if (gender === "Male") {
+    dataByKanton[kanton].male += 1;
+    dataByKanton["sum"].male += 1;
+  }
+  dataByKanton[kanton].both += 1;
+  dataByKanton["sum"].both += 1;
+}
+
+function assignData(data, variables) {
+  if (!data) {
+    data = variables;
+  }
+  return data;
+}
+
+function prepareMapData(map, dataByKanton) {
+    
+  map.features.forEach(function (d) {
+    d.properties.details = dataByKanton[d.properties.KantonName_de + " (" + d.properties.alternateName + ")"] ?
+        dataByKanton[d.properties.KantonName_de + " (" + d.properties.alternateName + ")"] : dataByKanton[d.properties.KantonName_fr + " (" + d.properties.alternateName + ")"] ?
+        dataByKanton[d.properties.KantonName_fr + " (" + d.properties.alternateName + ")"] : dataByKanton[d.properties.KantonName_it + " (" + d.properties.alternateName + ")"] ?
+        dataByKanton[d.properties.KantonName_it + " (" + d.properties.alternateName + ")"] : dataByKanton[d.properties.KantonName_en + " (" + d.properties.alternateName + ")"];
+
+});
+
+
+}
