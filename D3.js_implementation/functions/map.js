75f9351a19f42a51b9a35438c76f33565aaf67cf
MKulo <91249446+MKulo@users.noreply.github.com>
2023-05-27 20:30:01 +0200

    added coloring for the cantons based on variables (doesn't work completely because of different variable names (Zurich =! Zürich), ...)

--- D3.js_implementation/functions/map.js
+++ D3.js_implementation/functions/map.js
@@ -1,9 +1,93 @@
 // Renders the map and can then add the TREE-Data to it.
+
+// Creates an array of the form [{kanton : "Bern (BE)", value : 123}, {kanton : "Vaud (VD", value : 12}, ...] based on select
+function createColorArray(data, select){
+
+    var valueArray = [];
+    let dataByKanton = {};
+
+    // pretty sure there is a better way since we are repeating what we are doing in update map
+    data.forEach(function (d) {
+        let kanton = d["aes_canton"].includes("Basel") ? "beide Basel (BL/BS)" : d["aes_canton"];
+        const kantonToFind = kanton;
+        switch (select) {
+            case "gender" :
+                prepareGender(d, dataByKanton, kanton);
+                const foundKanton = valueArray.find(item => item.kanton === kantonToFind);
+                if(!foundKanton){
+                    valueArray.push({kanton: kanton, value: dataByKanton[kanton].female})
+                }
+                else{
+                    const updatedData = valueArray.map(item => {
+                        if (item.kanton === kanton) {
+                            return { ...item, value: dataByKanton[kanton].female+1 }; // Update the value property
+                        }
+                        return item;
+                    });
+                    valueArray = updatedData;
+                }
+                break;
+          case "immigration" :
+                break;
+          case "" : break;
+          default : break;
+        }
+      });
+    return valueArray;
+}
+
+function transformValuesToColors(valueArray) {
+  // Extract the values from the valueArray
+  const values = valueArray.map(item => item.value);
+
+  // Define the color scale
+  const colorScale = d3.scaleLinear()
+    .domain([Math.min(...values), Math.max(...values)])
+    .range(["#00ff00", "#0000ff"]); // Specify the color range
+
+  // Create an object with kantons and their corresponding colors
+  const kantonColors = {};
+  valueArray.forEach(item => {
+    const kanton = item.kanton;
+    const color = colorScale(item.value);
+    kantonColors[kanton] = color;
+  });
+
+    // Create a scale description
+  const scaleDescription = {
+    min: Math.min(...values),
+    max: Math.max(...values),
+    colors: ["#00ff00", "#0000ff"] // Specify the color range
+  };
+
+  return {
+    kantonColors,
+    scaleDescription
+  };
+}
+
+function getColor(colorArray, canton) {
+    for (const key in colorArray) {
+        console.log(key, canton);
+    if (key === canton) {
+      return colorArray[key];
+    }
+  }
+  return null;
+}
+
 function drawMap(map, data /*,index */) {
     let projection = d3.geoIdentity().reflectY(true).fitSize([width*0.9, height*0.9], map);// The projection determines what kind of plane the map itself is projected on to (eg. onto a globe or a flat plain).
     let path = d3.geoPath().projection(projection);
-    
+
     updateMap(map, data, "gender"); // Prepare the data that should be rendered (decided by user via selection panel)
+    let valueArray = createColorArray(data, "gender");
+    console.log(valueArray);
+    let colorArray = transformValuesToColors(valueArray).kantonColors;
+
+//    const colorScale = d3.scaleSequential()
+//        .domain([44, 351]) // Specify the range of values
+//        .interpolator(d3.interpolateViridis); // Choose a color palette or interpolation function
 
 
     let tooltip = d3.select("#map")
@@ -64,7 +148,7 @@ function drawMap(map, data /*,index */) {
             .attr("class", "canton")
             .attr("id", function(d) { return d.properties.KantonId; })
             .attr("d", path) 
-            .style("fill", "grey") // Set the background color of the Kantons
+            .style("fill", function(d) { console.log(getColor(colorArray, d.properties.KantonName_de + " (" + d.properties.alternateName + ")")); return getColor(colorArray, d.properties.KantonName_de + " (" + d.properties.alternateName + ")")}) // Set the background color of the Kantons
             .style("stroke", "white") // Set the color of the Kanton Borders
             .style("stroke-width", 0.5) // Thickness of the borderlines
             .style("z-index", "950")
@@ -82,7 +166,7 @@ function updateMap(map, data, select) {
     
         switch (select) {
           case "gender" : prepareGender(d, dataByKanton, kanton); break;
-          case "immigratoin" : prepareImmigration(d, dataByKanton, kanton); break;
+          case "immigration" : prepareImmigration(d, dataByKanton, kanton); break;
           case "" : break;
           default : break;
         }
@@ -152,6 +236,33 @@ function prepareGender(d, dataByKanton, kanton) {
   }
   dataByKanton[kanton].both += 1;
   dataByKanton["sum"].both += 1;
+}
+
+function prepareStatus(d, dataByKanton, kanton) {
+  let status = d["t0hisei08_3q"];
+
+  dataByKanton[kanton] = assignData(dataByKanton[kanton], {low: 0, medium: 0, high: 0,});
+
+  if (status === "Low") {
+    dataByKanton[kanton].low += 1;
+
+  } else if (status === "Medium") {
+    dataByKanton[kanton].medium += 1;
+  } else if (status === "High") {
+    dataByKanton[kanton].high += 1;
+  }
+}
+
+function prepareMathscore(d, dataByCanton, kanton){
+    let mathscore = d["t0wlem_3q"];
+    assignData(dataByKanton[kanton], {low_ms: 0, medium_ms: 0, high_ms: 0, missing: 0});
+
+  switch (mathscore) {
+    case "High": dataByKanton[kanton].high_ms += 1;
+    case "Medium": dataByKanton[kanton].medium_ms += 1;
+    case "Low": dataByKanton[kanton].low_ms += 1;
+    case ".": dataByKanton[kanton].missing_ms += 1;
+  }
 }
 
 function assignData(data, variables) {
